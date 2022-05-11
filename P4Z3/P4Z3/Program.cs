654f2c7cc7349bea6f11442fe532e26bd6be5789
kingorbit <ghause6@gmail.com>
2022-05-11 22:42:23 +0200

    update6

--- P4Z3/P4Z3/Program.cs
+++ P4Z3/P4Z3/Program.cs
@@ -1,5 +1,10 @@
-﻿using Dapper;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Data.SqlClient;
+using Dapper;
 
 
 string connection = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Northwind; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
